3c48e61bdb1f647ae37e4a2483dae05705aff6ca
Soumya D. Sanyal <soumya@soumyadsanyal.com>
2015-05-04 18:12:01 -0500

    added minus, divide, not, and, or

--- typed_lambda_calculus.hs
+++ typed_lambda_calculus.hs
@@ -4,8 +4,13 @@ data Exp where
  App :: Exp -> Exp -> Exp 
  Constant :: Value -> Exp 
  Plus :: Exp -> Exp -> Exp 
+ Minus :: Exp -> Exp -> Exp
  Times :: Exp -> Exp -> Exp 
+ Divide :: Exp -> Exp -> Exp 
  Variable :: Int -> Exp
+ Not :: Exp -> Exp
+ And :: Exp -> Exp -> Exp
+ Or :: Exp -> Exp -> Exp
 
 data Value = VInt Int | VBool Bool | VFunction (Value -> Value)
 
@@ -23,9 +28,28 @@ eval (Constant x) = x
 eval (Plus x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> VInt (x+y)
   _                -> error "Not integers, you dumbass!"
+eval (Minus x y) = case (eval x, eval y) of 
+  (VInt x, VInt y) -> VInt (x-y)
+  _                -> error "Not integers, you dumbass!"
 eval (Times x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> VInt (x*y)
   _                -> error "Not integers, you dumbass!"
+eval (Divide x y) = case (eval x, eval y) of 
+  (VInt x, VInt y) -> if (y/=0) then VInt (div x y) else error "Division by zero!"
+  _                -> error "Not integers, you dumbass!"
+eval (Not x) = case (eval x) of
+ (VBool x) -> VBool (not x)
+ _         -> error "Not Boolean"
+eval (And x y) = case (eval x, eval y) of
+ (VBool x, VBool y) -> VBool(x && y)
+ _         -> error "Not Boolean"
+eval (Or x y) = case (eval x, eval y) of
+ (VBool x, VBool y) -> VBool(x ||  y)
+ _         -> error "Not Boolean"
+
+
+
+
 
 eval (App first second) = case (eval first) of
  (VFunction f) -> f (eval second)
@@ -36,7 +60,12 @@ subst :: Exp -> Var -> Value -> Exp
 subst c@(Constant _) _ _ = c
 subst (Variable v) (Var v') x = if (v==v') then (Constant x) else (Variable v)
 subst (Plus m n) v x = Plus (subst m v x) (subst n v x)
+subst (Minus m n) v x = Minus (subst m v x) (subst n v x)
 subst (Times m n) v x = Times (subst m v x) (subst n v x)
+subst (Divide m n) v x = Divide (subst m v x) (subst n v x)
+subst (Not m) v x = Not (subst m v x)
+subst (And m n) v x = And (subst m v x) (subst n v x)
+subst (Or m n) v x = Or (subst m v x) (subst n v x)
 subst (App m n) v x = App (subst m v x) (subst n v x)
 subst (Fun v' b) v x = if (v==v') then (Fun v' b) else (Fun v' (subst b v x))
 
