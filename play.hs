fc88edd2365218c846178d272ac3caa0c93596f0
Soumya D. Sanyal <soumya@soumyadsanyal.com>
2015-05-04 21:16:56 -0500

    rename nats to customints

--- play.hs
+++ play.hs
@@ -13,34 +13,35 @@ data Exp where
  Or :: Exp -> Exp -> Exp
  Branch :: Exp -> Exp -> Exp -> Exp
 
-data Value = VNat Nat | VInt Int | VBool Bool | VFunction (Value -> Value)
+data Value = VCustomInt CustomInt | VInt Int | VBool Bool | VFunction (Value -> Value)
 
-data Nat = Zero | Succ Nat | Pred Nat
+data CustomInt = Zero | Succ CustomInt | Pred CustomInt
  deriving (Show, Eq)
 
-simplifynats :: Nat -> Nat
-simplifynats Zero = Zero
-simplifynats (Succ (Pred x)) = x
-simplifynats (Pred (Succ x)) = x
+simplifyCustomInts :: CustomInt -> CustomInt
+simplifyCustomInts Zero = Zero
+simplifyCustomInts (Succ (Pred x)) = x
+simplifyCustomInts (Pred (Succ x)) = x
+simplifyCustomInts x = x
 
-addnats :: Nat -> Nat -> Nat
-addnats x Zero = x
-addnats x (Succ y) = Succ (addnats x y)
-addnats x (Pred y) = Pred (addnats x y)
+customadd :: CustomInt -> CustomInt -> CustomInt
+customadd x Zero = simplifyCustomInts x
+customadd x (Succ y) = simplifyCustomInts (Succ (customadd (simplifyCustomInts x) (simplifyCustomInts y)))
+customadd x (Pred y) = simplifyCustomInts (Pred (customadd (simplifyCustomInts x) (simplifyCustomInts y)))
 
-minusnats:: Nat -> Nat -> Nat
-minusnats x Zero = x
-minusnats x (Succ y) = Pred (minusnats x y)
-minusnats x (Pred y) = Succ (minusnats x y)
+customsubtract:: CustomInt -> CustomInt -> CustomInt
+customsubtract x Zero = x
+customsubtract x (Succ y) = Pred (customsubtract x y)
+customsubtract x (Pred y) = Succ (customsubtract x y)
 
-timesnats :: Nat -> Nat -> Nat
-timesnats x Zero = Zero
-timesnats x (Succ y) = addnats x (timesnats x y)
-timesnats x (Pred y) = minusnats (timesnats x y) x
+custommultiply :: CustomInt -> CustomInt -> CustomInt
+custommultiply x Zero = Zero
+custommultiply x (Succ y) = customadd x (custommultiply x y)
+custommultiply x (Pred y) = customsubtract (custommultiply x y) x
 
 instance Show Value where
  show (VInt x) = show x
- show (VNat x) = show x
+ show (VCustomInt x) = show x
  show (VBool x) = show x
  show _ = "<Function>"
 
@@ -52,27 +53,27 @@ eval :: Exp -> Value
 eval (Constant x) = x
 eval (Plus x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> VInt (x+y)
-  (VNat x, VNat y) -> VNat (addnats x y)
-  (VInt _, VNat _) -> error "Incompatible argument types!"
-  (VNat _, VInt _) -> error "Incompatible argument types!"
-  _                -> error "Arguments must be ints or nats!"
+  (VCustomInt x, VCustomInt y) -> VCustomInt (customadd x y)
+  (VInt _, VCustomInt _) -> error "Incompatible argument types!"
+  (VCustomInt _, VInt _) -> error "Incompatible argument types!"
+  _                -> error "Arguments must be Ints or CustomInts!"
 eval (Minus x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> VInt (x-y)
-  (VNat x, VNat y) -> VNat (minusnats x y)
-  (VInt _, VNat _) -> error "Incompatible argument types!"
-  (VNat _, VInt _) -> error "Incompatible argument types!"
-  _                -> error "Arguments must be ints or nats!"
+  (VCustomInt x, VCustomInt y) -> VCustomInt (customsubtract x y)
+  (VInt _, VCustomInt _) -> error "Incompatible argument types!"
+  (VCustomInt _, VInt _) -> error "Incompatible argument types!"
+  _                -> error "Arguments must be Ints or CustomInts!"
 eval (Times x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> VInt (x*y)
-  (VNat x, VNat y) -> VNat (timesnats x y)
-  (VInt _, VNat _) -> error "Incompatible argument types!"
-  (VNat _, VInt _) -> error "Incompatible argument types!"
-  _                -> error "Arguments must be ints or nats!"
+  (VCustomInt x, VCustomInt y) -> VCustomInt (custommultiply x y)
+  (VInt _, VCustomInt _) -> error "Incompatible argument types!"
+  (VCustomInt _, VInt _) -> error "Incompatible argument types!"
+  _                -> error "Arguments must be Ints or CustomInts!"
 eval (Divide x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> if (y/=0) then VInt (div x y) else error "Division by zero!"
-  (VNat x, VNat y) -> error "Division not defined on nats!"
-  (VInt _, VNat _) -> error "Incompatible argument types!"
-  (VNat _, VInt _) -> error "Incompatible argument types!"
+  (VCustomInt x, VCustomInt y) -> error "Division not defined on CustomInts!"
+  (VInt _, VCustomInt _) -> error "Incompatible argument types!"
+  (VCustomInt _, VInt _) -> error "Incompatible argument types!"
   _                -> error "Arguments must be integers!"
 eval (Not x) = case (eval x) of
  (VBool x) -> VBool (not x)
@@ -91,6 +92,7 @@ eval (App first second) = case (eval first) of
  _ ->  error "First argument is not a function!"
 eval (Fun v f) = VFunction (\x -> eval(subst f v x))
 
+
 subst :: Exp -> Var -> Value -> Exp
 subst c@(Constant _) _ _ = c
 subst (Variable v) (Var v') x = if (v==v') then (Constant x) else (Variable v)
@@ -104,7 +106,7 @@ subst (Or m n) v x = Or (subst m v x) (subst n v x)
 subst (App m n) v x = App (subst m v x) (subst n v x)
 subst (Fun v' b) v x = if (v==v') then (Fun v' b) else (Fun v' (subst b v x))
 
-plusone = Fun (Var 1) (Plus (Variable 1) (Constant (VNat (Succ Zero))))
+plusone = Fun (Var 1) (Plus (Variable 1) (Constant (VCustomInt (Succ Zero))))
 
 y=Fun (Var 1) (App ((Fun (Var 2) (App (Variable 1) (App (Variable 2) (Variable 2))))) (Fun (Var 3) (App (Variable 1) (App (Variable 3) (Variable 3)))))
 
