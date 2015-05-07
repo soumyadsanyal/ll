1b52cf03341a8579b31852c2d7edfaf78be282c9
Soumya Deepta Sanyal <sds2p8@missouri.edu>
2015-05-06 20:00:31 -0500

    final version we think

--- chris.hs
+++ chris.hs
@@ -9,196 +9,103 @@ data Exp where
  Minus :: Exp -> Exp -> Exp
  Times :: Exp -> Exp -> Exp 
  Divide :: Exp -> Exp -> Exp 
- Variable :: Int -> Annotation -> Exp
+ Variable :: Int -> Exp
  Not :: Exp -> Exp
  And :: Exp -> Exp -> Exp
  Or :: Exp -> Exp -> Exp
  Branch :: Exp -> Exp -> Exp -> Exp
 
-data Value = VBool' Bool'  | VInt' Int' | VInt Int | VBool Bool | VFunction (Value -> Value)
+data Value = VNat Nat | VInt Int | VBool Bool | VFunction (Value -> Value) 
 
 --Custom datatypes. These will support ad-hoc polymorphism alongside their usual counterparts below.
 
-data Int' = Zero | Succ Int' | Pred Int'
- deriving (Show, Eq)
-
-data Bool' = True' | False'
+data Nat = Zero | Succ Nat 
  deriving (Show, Eq)
 
 data Parity = P | S 
 
 instance Show Value where
  show (VInt x) = show x
- show (VInt' x) = show x
+ show (VNat x) = show x
  show (VBool x) = show x
- show (VBool' x) = show x
  show _ = "<Function>"
 
-data Var = Var Int Annotation
+data Var = Var Int Type
  deriving (Eq, Show)
 
-data Annotation = AInt | ABool | AInt' | ABool' | AFunction Annotation Annotation 
+data Type = TInt | TBool | TNat | TFunction Type Type  | TError
  deriving (Eq, Show)
 
 
--- reducer, reduce, expand and simplify reduce any Int' to a canonical form. This solution only uses the List datatype provided by Haskell.
-reducer :: Int' -> Int'
-reducer Zero = Zero
-reducer (Succ x) = case x of
- (Succ y) -> Succ (reducer x)
- (Pred y) -> reducer y
- Zero -> Succ Zero
-reducer (Pred x) = case x of
- (Succ y) -> reducer y
- (Pred y) -> Pred (reducer x)
- Zero -> Pred Zero
+-- reducer, reduce, expand and simplify reduce any Nat to a canonical form. This solution only uses the List datatype provided by Haskell.
 
-expand :: [Parity] -> Int' -> Int'
-expand [] x = x
-expand (P:rest) x = expand rest (Pred x)
-expand (S:rest) x = expand rest (Succ x)
+-- provide operations for Nats. Nats with these operations form a ring isomorphic to the integers. It might be fun to implement the field of fractions.
+add' :: Nat -> Nat -> Nat
+add' x Zero = x
+add' x (Succ y) = (Succ (add' (x) (y)))
 
-reduce :: [Parity] -> (Int' -> Int') -> Int' -> Int'
---base cases
-reduce [] reducer Zero = Zero 
-reduce [] reducer (Succ x) = reduce (S:[]) reducer x
-reduce [] reducer (Pred x) = reduce (P:[]) reducer x
-reduce (S:rest) reducer Zero = expand rest (Succ Zero)
-reduce (P:rest) reducer Zero = expand rest (Pred Zero)
---inductive cases
-reduce (S:rest) reducer (Pred x) = reduce rest reducer x
-reduce (P:rest) reducer (Succ x) = reduce rest reducer x
-reduce (S:rest) reducer (Succ x) = reduce (S:S:rest) reducer x
-reduce (P:rest) reducer (Pred x) = reduce (P:P:rest) reducer x
-
-simplify :: Int' -> Int'
-simplify x = reduce [] reducer x
--- makelist', reduce', expand', simplify' reduce any Int' to canonical form. This alternative solution uses the List and Int types in Haskell.
-makelist' :: Int' -> [Char]
-makelist' Zero = []
-makelist' (Succ x) = 'S':(makelist' x)
-makelist' (Pred x) = 'P':(makelist' x)
-
-reduce' :: [Char] -> Int
-reduce' [] = 0
-reduce' ('S':rest) = 1+(reduce' rest)
-reduce' ('P':rest) = (reduce' rest) - 1
-
-expand' :: Int -> Int'
-expand' n 
- | n==0 = Zero
- | n>0 = Succ (expand' (n-1))
- | n<0 = Pred (expand' (n+1))
-
-simplify' :: Int' -> Int' 
-simplify' x = expand' (reduce' (makelist' x)) 
-
-
--- provide operations for Int's. Int's with these operations form a ring isomorphic to the integers. It might be fun to implement the field of fractions.
-add' :: Int' -> Int' -> Int'
-add' x Zero = simplify x
-add' x (Succ y) = simplify (Succ (add' (x) (y)))
-add' x (Pred y) = simplify (Pred (add' x y))
-
-minus' :: Int' -> Int' -> Int'
-minus' x Zero = simplify x
-minus' x (Succ y) = simplify (Pred (minus' (x) (y)))
-minus' x (Pred y) = simplify (Succ (minus' x y))
-
-
-times' :: Int' -> Int' -> Int'
+times' :: Nat -> Nat -> Nat
 times' x Zero = Zero
-times' x (Succ y) = simplify (add' x (times' x y))
-times' x (Pred y) = simplify (minus' (times' x y) x)
+times' x (Succ y) = (add' x (times' x y))
 
--- provide operations for Bool'. This gives join, meet and complementation.
-and' :: Bool' -> Bool' -> Bool'
-and' True' True' = True'
-and' _ _ = False'
-
-or' :: Bool' -> Bool' -> Bool'
-or' False' False' = False'
-or' _ _ = True'
-
-not' :: Bool' -> Bool' 
-not' True' = False'
-not' False' = True'
-
---Functions to extract Int's and Bool's from the Value wrapper. Along with translateint and translatebool below, this allows me to map customtypes to their usual counterparts, for the purposes of the talk.
-unwrapint :: Value -> Int'
+--Functions to extract Nats and Bool's from the Value wrapper. Along with translateint and translatebool below, this allows me to map customtypes to their usual counterparts, for the purposes of the talk.
+unwrapint :: Value -> Nat
 unwrapint x = case x of
- (VInt' Zero) -> Zero
- (VInt' (Succ x)) -> Succ (x)
- (VInt' (Pred x)) -> Pred (x)
+ (VNat Zero) -> Zero
+ (VNat (Succ x)) -> Succ (x)
 
-unwrapbool :: Value -> Bool'
-unwrapbool x = case x of
- (VBool' True') -> True'
- (VBool' False') -> False'
-
-translateint :: Int' -> Int
+translateint :: Nat -> Int
 translateint Zero = 0
 translateint (Succ x) = (translateint x) + 1
-translateint (Pred x) = (translateint x) - 1
-
-translatebool :: Bool' -> Bool
-translatebool True' = True
-translatebool False' = not (translatebool (not' False'))
 
 -- just as a comment, eval is a functor!
 eval :: Exp -> Value
 eval (Constant x) = x
 eval (Plus x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> VInt (x+y)
-  (VInt' x, VInt' y) -> VInt' (add' (simplify x) (simplify y))
-  (VInt _, VInt' _) -> error "Incompatible argument types!"
-  (VInt' _, VInt _) -> error "Incompatible argument types!"
-  _                -> error "Arguments must be Ints or Int's!"
+  (VNat x, VNat y) -> VNat (add' (x) (y))
+  (VInt _, VNat _) -> error "Incompatible argument types!"
+  (VNat _, VInt _) -> error "Incompatible argument types!"
+  _                -> error "Arguments must be Ints or Nats!"
 eval (Minus x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> VInt (x-y)
-  (VInt' x, VInt' y) -> VInt' (minus' (simplify x) (simplify y))
-  (VInt _, VInt' _) -> error "Incompatible argument types!"
-  (VInt' _, VInt _) -> error "Incompatible argument types!"
+  (VNat x, VNat y) -> VInt ((translateint x) - (translateint y))
+  (VInt _, VNat _) -> error "Incompatible argument types!"
+  (VNat _, VInt _) -> error "Incompatible argument types!"
   _                -> error "Arguments must be Ints!"
 eval (Times x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> VInt (x*y)
-  (VInt' x, VInt' y) -> VInt' (times' (simplify x) (simplify y))
-  (VInt _, VInt' _) -> error "Incompatible argument types!"
-  (VInt' _, VInt _) -> error "Incompatible argument types!"
-  _                -> error "Arguments must be Ints or Int's!"
+  (VNat x, VNat y) -> VNat (times' (x) (y))
+  (VInt _, VNat _) -> error "Incompatible argument types!"
+  (VNat _, VInt _) -> error "Incompatible argument types!"
+  _                -> error "Arguments must be Ints or Nats!"
 eval (Divide x y) = case (eval x, eval y) of 
   (VInt x, VInt y) -> if (y/=0) then VInt (div x y) else error "Division by zero!"
-  (VInt' x, VInt' y) -> error "Division not defined on Int's!"
-  (VInt _, VInt' _) -> error "Incompatible argument types!"
-  (VInt' _, VInt _) -> error "Incompatible argument types!"
+  (VNat x, VNat y) -> if (y/= Zero) then VInt (div (translateint x) (translateint y)) else error "Division by zero!"
+  (VInt _, VNat _) -> error "Incompatible argument types!"
+  (VNat _, VInt _) -> error "Incompatible argument types!"
   _                -> error "Arguments must be integers!"
 eval (Not x) = case (eval x) of
  (VBool x) -> VBool (not x)
- (VBool' x) -> VBool' (not' x)
  _        -> error "Not Boolean!"
 eval (And x y) = case (eval x, eval y) of
  (VBool x, VBool y) -> VBool(x && y)
- (VBool' x, VBool' y) -> VBool' (and' x y)
  _         -> error "Not Boolean!"
 eval (Or x y) = case (eval x, eval y) of
  (VBool x, VBool y) -> VBool(x ||  y)
- (VBool' x, VBool' y) -> VBool' (or' x y)
  _         -> error "Not Boolean!"
 eval (Branch x y z) = case (eval x, eval y, eval z) of
  (VBool x, this, that) -> if (x==True) then this else that
- (VBool' x, this, that) -> if (x==True') then this else that
  _         -> error "Condition must evaluate to Bool or Bool'!"
 eval (App first second) = case (eval first) of
  (VFunction f) -> f (eval second)
  _ ->  error "First argument is not a function!"
-eval (Fun v f) = VFunction (\x -> eval(subst f v x))
+eval (Fun (Var v _) f) = VFunction (\x -> eval(subst f v x))
 
 
-subst :: Exp -> Var -> Value -> Exp
+subst :: Exp -> Int -> Value -> Exp
 subst c@(Constant _) _ _ = c
-subst (Variable v annotation) (Var v' annotation') x 
- | annotation == annotation' = if (v==v') then (Constant x) else (Variable v annotation)
- | otherwise = error "annotations don't match in the variable"
+subst (Variable v ) (v' ) x = if (v==v') then (Constant x) else (Variable v )
 subst (Plus m n) v x = Plus (subst m v x) (subst n v x)
 subst (Minus m n) v x = Minus (subst m v x) (subst n v x)
 subst (Times m n) v x = Times (subst m v x) (subst n v x)
@@ -207,20 +114,64 @@ subst (Not m) v x = Not (subst m v x)
 subst (And m n) v x = And (subst m v x) (subst n v x)
 subst (Or m n) v x = Or (subst m v x) (subst n v x)
 subst (App m n) v x = App (subst m v x) (subst n v x)
-subst (Fun (Var v' annotation') b) (Var v annotation) x 
- | annotation == annotation' = if (v==v') then (Fun (Var v' annotation')  b) else (Fun (Var v' annotation') (subst b (Var v annotation) x))
- | otherwise = error "annotations don't match in the Fun"
+subst (Fun (Var v' t') b) (v ) x = if (v==v') then (Fun (Var v' t')  b) else (Fun (Var v' t') (subst b (v ) x))
 
-plusone = Fun (Var 1 AInt') (Plus (Variable 1 AInt') (Constant (VInt' (Succ Zero))))
-timesfour = Fun (Var 1 AInt') (Times (Variable 1 AInt') (Constant (VInt' (Succ (Succ (Succ (Succ Zero)))))))
+evalT :: (Int -> Type) -> Exp -> Type
+evalT _ (Constant (VInt _)) = TInt  
+evalT _ (Constant (VBool _)) = TBool
+evalT _ (Constant (VNat _)) = TNat
+evalT _ (Constant _) = TError
+evalT tenv (Variable v) = tenv v
+evalT tenv (Plus x y) = case (evalT tenv x, evalT tenv y) of 
+  (TInt, TInt ) -> TInt
+  (TNat , TNat ) -> TNat 
+  _ -> TError
+evalT tenv (Minus x y) = case (evalT tenv x, evalT tenv y) of 
+  (TInt , TInt ) -> TInt
+  (TNat , TNat ) -> TInt
+  _ -> TError
+evalT tenv (Times x y) = case (evalT tenv x, evalT tenv y) of 
+  (TInt , TInt ) -> TInt
+  (TNat , TNat ) -> TNat
+  _                -> TError
+evalT tenv (Divide x y) = case (evalT tenv x, evalT tenv y) of 
+  (TInt , TInt ) -> TInt
+  (TNat, TNat) -> TInt
+  _                -> TError 
+evalT tenv (Not x) = case (evalT tenv x) of
+ (TBool ) -> TBool 
+ _        -> TError
+evalT tenv (And x y) = case (evalT tenv x, evalT tenv y) of
+ (TBool , TBool ) -> TBool
+ _         -> TError
+evalT tenv (Or x y) = case (evalT tenv x, evalT tenv y) of
+ (TBool , TBool ) -> TBool
+ _         -> TError 
+evalT tenv (Branch x y z) = case (evalT tenv x, evalT tenv y, evalT tenv z) of
+ (TBool , this, that) -> if (this==that) then this else TError
+ _         -> TError 
+evalT tenv (App f x) = case (evalT tenv f) of
+ (TFunction a b) -> if (a==(evalT tenv x)) then b else TError
+ _ ->  TError 
+evalT tenv (Fun (Var v t) f) = TFunction t (evalT (extend tenv (v) t) f)
+
+extend :: Eq a => (a -> b) -> a -> b -> (a -> b)
+extend f x y = \w -> if (w==x) then y else (f w)
+
+eval' :: Exp -> Value
+eval' e = if (evalT initialenv e /= TError) then eval e else error "Type error!"
+ where
+  initialenv _ = TError
+
+plusone = Fun (Var 1 TNat) (Plus (Variable 1 ) (Constant (VNat (Succ Zero))))
+timesfour = Fun (Var 1 TNat) (Times (Variable 1 ) (Constant (VNat (Succ (Succ (Succ (Succ Zero)))))))
 
 
-y=Fun (Var 1 AInt') (App ((Fun (Var 2 AInt') (App (Variable 1 AInt') (App (Variable 2 AInt') (Variable 2 AInt'))))) (Fun (Var 3 AInt') (App (Variable 1 AInt') (App (Variable 3 AInt') (Variable 3 AInt')))))
+y=Fun (Var 1 TNat) (App ((Fun (Var 2 TNat) (App (Variable 1 ) (App (Variable 2 ) (Variable 2 ))))) (Fun (Var 3 TNat) (App (Variable 1 ) (App (Variable 3 ) (Variable 3 )))))
 
-test = translateint (unwrapint (eval (Times (Constant (VInt' (Succ (Pred (Succ (Pred (Succ (Succ Zero)))))))) (Constant (VInt' (Pred Zero))))))
 
-double = Fun (Var 1 (AFunction AInt' AInt')) (
-             Fun (Var 2 AInt') (
-                 App (Variable 1 (AFunction AInt' AInt')) (
-                      App (Variable 1 (AFunction AInt' AInt')) (
-                              (Variable 2 AInt')))))
+double = Fun (Var 1 (TFunction TNat TNat)) (
+             Fun (Var 2 TNat) (
+                 App (Variable 1 ) (
+                      App (Variable 1 ) (
+                              (Variable 2 )))))
